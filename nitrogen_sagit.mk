617729f88033c0b219e7d5abbfb0e3c489b84ae3
wloot <wlootlxt123@gmail.com>
2018-08-23 18:27:24 +0800

    Revert "sagit: remove full_sagit.mk"
    This reverts commit fc6061d7ecc69b5203f9ae581ddfa0ef0c1579d3.

--- nitrogen_sagit.mk
+++ nitrogen_sagit.mk
@@ -1,7 +1,4 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
+$(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 $(call inherit-product, vendor/nitrogen/products/common.mk)
 
 PRODUCT_NAME := nitrogen_sagit
